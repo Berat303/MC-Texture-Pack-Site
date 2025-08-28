8df38f0b4fc01149194281c0e6368cb3926cb274
Berat303 <cakirberat303@gmail.com>
2025-08-28 20:26:31 +0300

    Add files via upload

---
+++ mySiteyedek/public/png/js/ytig.js
@@ -0,0 +1,17 @@
+const youtubeIcon =
+document.querySelector(' .fa-brands.fa-square-youtube').addEventListener('click', function(){
+    if(youtubeIcon){
+        youtubeIcon.style.cursor = "pointer";
+        youtubeIcon.addEventListener('click', () => {
+
+        })
+    }
+
+    window.open("https://www.youtube.com/@kyuinaa?si=ngIMODVHm2Z9j1G8", "blank");
+
+})
+
+
+document.querySelector(' .fa-square-instagram').addEventListener('click', function(){
+    window.open("", "blank");
+})
